9d7f0cf161843c0df1584412d329fba6cfda37ac
Valentina Cupać <valentina.cupac@optivem.com>
2022-06-25 11:19:58 +0200

    #2 Use Case - Calculate Discount

--- src/main/java/com/optivem/kata/hexarch/application/App.java
+++ src/main/java/com/optivem/kata/hexarch/application/App.java
@@ -1,8 +1,6 @@
 package com.optivem.kata.hexarch.application;
 
-import java.math.BigDecimal;
-
-public class Application {
+public class App {
     public double calculateDiscount(double amount) {
         var value = amount * 0.05;
         return value;
--- src/test/java/com/optivem/kata/hexarch/application/AppTest.java
+++ src/test/java/com/optivem/kata/hexarch/application/AppTest.java
@@ -1,18 +1,16 @@
 package com.optivem.kata.hexarch.application;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.math.BigDecimal;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ApplicationTest {
-    private Application application;
+public class AppTest {
+    private App app;
 
     private static Stream<Arguments> should_calculate_discount_given_positive_amount() {
         return Stream.of(Arguments.of(100.0, 5.0));
@@ -20,13 +18,13 @@ public class ApplicationTest {
 
     @BeforeEach
     private void init() {
-        application = new Application();
+        app = new App();
     }
 
     @ParameterizedTest
     @MethodSource
     void should_calculate_discount_given_positive_amount(double amount, double expectedDiscountedAmount) {
-        var discountedAmount = application.calculateDiscount(amount);
+        var discountedAmount = app.calculateDiscount(amount);
         assertThat(discountedAmount).isEqualTo(expectedDiscountedAmount);
     }
 }
