f0bd8d1fb943517801e01c3edf651223779f0930
Valentina Cupać <valentina.cupac@optivem.com>
2022-06-25 11:45:32 +0200

    #2 Use Case - Calculate Discount

--- src/test/java/com/optivem/kata/hexarch/application/CalculateDiscountTest.java
+++ src/test/java/com/optivem/kata/hexarch/application/CalculateDiscountTest.java
@@ -10,7 +10,7 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AppTest {
+public class CalculateDiscountTest {
     private FakeDiscountRateRepository rateRepository;
     private App app;
 
