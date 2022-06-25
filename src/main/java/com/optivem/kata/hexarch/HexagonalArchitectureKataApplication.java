5e4aed6fb9ef3d4ab0d2be0944c93c48f769fa07
Valentina Cupać <valentina.cupac@optivem.com>
2022-06-25 10:09:50 +0200

    #1 Setup Project - Created from Spring Boot Template

---
+++ src/main/java/com/optivem/kata/hexarch/HexagonalArchitectureKataApplication.java
@@ -0,0 +1,13 @@
+package com.optivem.kata.hexarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalArchitectureKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/optivem/kata/hexarch/HexagonalArchitectureKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optivem.kata.hexarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
