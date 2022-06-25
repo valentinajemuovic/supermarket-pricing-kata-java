ccbaa09534cdb7ab994b18f6bd901ad5291307d3
Valentina Cupać <valentina.cupac@optivem.com>
2022-06-25 10:52:40 +0200

    #2 Use Case - Calculate Discount

--- src/test/java/com/optivem/kata/hexarch/application/ApplicationTest.java
+++ src/test/java/com/optivem/kata/hexarch/application/ApplicationTest.java
@@ -2,23 +2,31 @@ package com.optivem.kata.hexarch.application;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ApplicationTest {
     private Application application;
 
+    private static Stream<Arguments> should_calculate_discount_given_positive_amount() {
+        return Stream.of(Arguments.of(100.0, 5.0));
+    }
+
     @BeforeEach
     private void init() {
         application = new Application();
     }
 
-    @Test
-    void should_calculate_discount_given_positive_amount() {
-        var amount = 100.0;
+    @ParameterizedTest
+    @MethodSource
+    void should_calculate_discount_given_positive_amount(double amount, double expectedDiscountedAmount) {
         var discountedAmount = application.calculateDiscount(amount);
-        assertThat(discountedAmount).isEqualTo(5);
+        assertThat(discountedAmount).isEqualTo(expectedDiscountedAmount);
     }
 }
